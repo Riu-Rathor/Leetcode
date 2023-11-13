a4721434132325013a36c38311d1a086c985ca18
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-11-13 14:57:00 +0530

    Time: 28 ms (95.22%) | Memory: 11.6 MB (96.84%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    string sortVowels(string s) {
+
+    bool isVowel(char ch) {
+         if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+            return false;
+    }
+    string sortVowels(string s) {        
+        unordered_map<char, int> mp;
         string vowel = "AEIOUaeiou";
         int n = s.length();
-        unordered_map<char, int> mp;
         for(int i=0; i<n; i++) {
-            mp[s[i]]++;            
+            if(isVowel(s[i])) {
+                mp[s[i]]++;
+            }
+                      
         }
 
         int j = 0;
         for(int i=0; i<n; i++) {
-            if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' ||
-            s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            if(isVowel(s[i])) {
                 while(true) {
                     if( mp[vowel[j]] > 0) {
                         s[i] = vowel[j];
