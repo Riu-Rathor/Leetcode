cfad96ea176dc04770ab872ff6e88631007b509d
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-11-19 10:32:45 +0530

    Time: 161 ms (62.88%) | Memory: 83 MB (88.96%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
         int n = nums.size();
-        int element = 0;
         int ans = 0;
         sort(nums.begin(), nums. end());
         for(int i=n-1; i>=1; i--) {
             int largest = nums[i];
             int newLargest = nums[i-1];
             if(largest != newLargest) {
-                ans = ans + element + 1;
+                ans += (n-i);
             }
-            element++;
         }
         return ans;
      }
