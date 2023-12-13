687e80a4cb73fdc0ee6875bbcaf444cdb71e83ad
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-13 07:34:46 +0530

    Time: 17 ms (54.47%) | Memory: 13.4 MB (25.79%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-
-    bool isSpecial(int row, int col, int n, int m, vector<vector<int>> &mat) {
-        for(int i=0; i<n; i++) {
-            if(mat[i][col] == 1 && i != row) {
-                return false;
-            }
-        }
-        for(int j=0; j<m; j++) {
-            if(mat[row][j] == 1 && j != col) {
-                return false;
-            }
-        }
-        return true;
-    }
     int numSpecial(vector<vector<int>>& mat) {
+        int ans = 0;
+
         int n = mat.size();
         int m = mat[0].size();
-        int ans = 0;
+        vector<int> rowCount(n, 0);
+        vector<int> colCount(m, 0);
+
+
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(mat[i][j] == 0) {
-                    continue;
-                }
                 if(mat[i][j] == 1) {
-                    if(isSpecial(i,j,n,m,mat)) {
-                        ans++; 
-                    }
-                    else {
-                        break;
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(mat[i][j] == 1) {
+                    if(rowCount[i] == 1 && colCount[j] == 1) {
+                        ans++;
                     }
                 }
             }
         }
         return ans;
+
     }
 };
