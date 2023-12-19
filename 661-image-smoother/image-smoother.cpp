951bea444fd4beba6c5e972d37453c71dc935b7f
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-19 20:23:12 +0530

    Time: 47 ms (34.77%) | Memory: 21.9 MB (92.62%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -1,65 +1,69 @@
 class Solution {
-
-    private:
-    int smootherAverage(int i, int j, vector<vector<int>>& img, int n, int m) {
-        int sum = img[i][j];
-        int cnt = 1;
-        if((i-1) >= 0 && (j-1) >= 0) {
-            sum += img[i-1][j-1];
-            cnt++;
-        }
-
-        if((i-1)>= 0) {
-            sum += img[i-1][j];
-            cnt++;
-        }
-
-        if((i-1) >= 0 && (j+1) < m) {
-            sum += img[i-1][j+1];
-            cnt++;
-        }
-
-        if((j-1)>= 0) {
-            sum += img[i][j-1];
-            cnt++;
-        }
-
-        if((j+1)< m) {
-            sum += img[i][j+1];
-            cnt++;
-        }
-
-        if((i+1) < n && (j-1) >= 0) {
-            sum += img[i+1][j-1];
-            cnt++;
-        }
-
-        if((i+1) < n) {
-            sum += img[i+1][j];
-            cnt++;
-        }
-
-        if((i+1) < n  && (j+1) < m) {
-            sum += img[i+1][j+1];
-            cnt++;
-        }
-
-        int avg = floor(sum / cnt);
-        return avg;
-
-        
-    }
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        int n = img.size();
-        int m = img[0].size();
-        vector<vector<int>> ans(n, vector<int> (m));
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                ans[i][j] = smootherAverage(i, j, img, n, m);
+        int m = img.size();
+        int n = img[0].size();
+        vector<int> prev(n);
+        int topCorner = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                int sum = 0;
+                int count = 0;
+                // adding bottom corners
+                if(i+1<m) {
+                    if(j-1>=0) {
+                        sum += img[i+1][j-1];
+                        count++;
+                    }
+                    sum += img[i+1][j];
+                    count++;
+                    if(j+1<n) {
+                        sum += img[i+1][j+1];
+                        count++;
+                    }
+                }
+
+                // right neighbour
+                if(j+1<n) {
+                    sum += img[i][j+1];
+                    count++;
+                }
+
+                // left neightbout
+
+                if(j-1 >= 0) {
+                    sum += prev[j-1];
+                    count++;
+                }
+                // current cell
+                sum += img[i][j];
+                count++;
+
+                // top neighbours
+                if(i-1 >= 0) {
+                    if(j-1 >= 0) {
+                        sum += topCorner;
+                        count++;
+                    }
+
+                    sum += prev[j];
+                    count++;
+
+                    if(j+1 < n) {
+                        sum += prev[j+1];
+                        count++;
+                    }
+                }
+
+                
+                if(i-1>=0) {
+                    topCorner = prev[j];
+                }
+                
+                prev[j] = img[i][j];
+                img[i][j] = sum / count;                
             }
         }
-        return ans;
+        return img;
     }
 };
