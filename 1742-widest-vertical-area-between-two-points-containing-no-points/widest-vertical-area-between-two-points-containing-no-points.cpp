8feb9e7fc3fd55684805047b8b83e36bd482def2
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-21 07:43:02 +0530

    Time: 289 ms (5.32%) | Memory: 66.7 MB (61.45%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        vector<int> pointsX;
-        for(int i=0; i<points.size(); i++) {
-            pointsX.push_back(points[i][0]);
+        sort(points.begin(), points.end());
+        int ans = 0;
+        
+        for(int i=1; i<points.size(); i++) {
+            ans = max(ans, points[i][0] - points[i-1][0]);
         }
-
-        sort(pointsX.begin(), pointsX.end());
-        int maxi = INT_MIN;
-        for(int i=1; i<pointsX.size(); i++) {
-            int diff = pointsX[i] - pointsX[i-1];
-            maxi = max(maxi, diff);
-        }
-        return maxi;
+        return ans;
     }
 };
