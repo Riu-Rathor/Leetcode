5b4538d9d5ab882b9f4f2581f89fd7e75cd16f5c
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-26 11:35:55 +0530

    Time: 42 ms (22.23%) | Memory: 11 MB (12.98%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -68,10 +68,33 @@ public:
                         ans = (ans + dp[dice-1][target-i]) % MOD;
                     }
                 }
-                 dp[dice][target] =  ans;
+                dp[dice][target] =  ans;
             }
         }
         return dp[n][t];
+    }
+
+    // Space Optimization
+    int solveSO(int n, int k, int t) {
+        
+        vector<int> prev(t+1, 0);
+        vector<int> curr(t+1, 0);
+        prev[0] = 1;
+
+        for(int dice=1; dice<=n; dice++) {
+            for(int target=1; target<=t; target++) {
+                int ans = 0;
+                for(int i=1; i<=k; i++) {
+                    if(target-i >= 0) {
+                        ans = (ans + prev[target-i]) % MOD;
+                    }
+                }
+                curr[target] =  ans;
+            }
+            prev = curr;
+
+        }
+        return prev[t];
     }
 
     int numRollsToTarget(int n, int k, int target) {
