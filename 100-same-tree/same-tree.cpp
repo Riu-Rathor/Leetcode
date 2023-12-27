ce6a58ef0ca8c00d5efa05d5650cadcdcbf0df6c
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-27 11:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (30.48%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -16,7 +16,7 @@ public:
             return true;
         }
 
-        if(!p || !q || p->val != q->val) {
+        if((p == NULL && q != NULL) || (p != NULL && q == NULL)|| (p->val != q->val)) {
             return false;
         }
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
