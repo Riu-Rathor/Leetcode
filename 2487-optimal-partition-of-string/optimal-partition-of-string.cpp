41fe15ef09649c91370d57d6098afad6bfd05be5
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-27 20:21:53 +0530

    Time: 28 ms (77.92%) | Memory: 10.8 MB (67.63%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int  freq[26] = {0};
+        // int freq[26] = {0};
+        vector<int> v;
         int ans = 1;
         for(int i=0; i<s.length(); i++) {
             int charToDigit = s[i] - 'a';
-            if(freq[charToDigit] != 0) {
+            // if(freq[charToDigit] != 0) {
+            if(find(v.begin(), v.end(), s[i]) != v.end()) {
                 ans++;
-                fill(begin(freq), end(freq), 0);
-                freq[charToDigit]++;
+                // fill(begin(freq), end(freq), 0);
+                v.clear();
+                // freq[charToDigit]++;
+                v.push_back(s[i]);
             }
             else {
-                freq[charToDigit]++;
+                v.push_back(s[i]);
             }
         }
         return ans;
