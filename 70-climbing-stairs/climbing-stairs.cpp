854b0ac04577f38f88d2e5390a97d8f289665714
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-28 12:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (34.15%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -14,21 +14,29 @@ public:
 
     // memoization
     int solveMem(int n, vector<int> &dp) {
-        if(n == 0) {
+        if(n == 0 || n == 1) {
             return 1;
-        } 
-        if(n < 0) {
-            return 0;
         }
         if(dp[n] != -1) {
             return dp[n];
         }
-        return dp[n] =  solveMem(n-1, dp) + solveMem(n-2, dp);
-        
+        return dp[n] =  solveMem(n-1, dp) + solveMem(n-2, dp);        
+    }
+
+    // tabular method
+    int solveTab(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] =  dp[i-1] + dp[i-2];  
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
         // return solve(n);
-        return solveMem(n, dp);
+        // return solveMem(n, dp);
+        return solveTab(n);
     }
 };
