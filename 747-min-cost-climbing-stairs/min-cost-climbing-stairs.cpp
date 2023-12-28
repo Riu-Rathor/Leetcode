a47b3703630da2883bef9221e88e73c78b9e3c00
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-28 21:39:04 +0530

    Time: 9 ms (19.90%) | Memory: 14.4 MB (7.53%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -24,11 +24,34 @@ class Solution {
         int twoStep = cost[i] + solveMem(i+2, n, cost, dp);
         return dp[i] =  min(oneStep, twoStep);
     }
+
+    // Tabular
+    int solveTab(int i, int &n, vector<int> &cost) {
+        vector<int> dp(n+1, 0);
+        dp[n-1] = cost[n-1];
+        
+        for(int i=n-2; i>=0; i--) {
+            int oneStep = 0;
+            int twoStep = 0;
+            if(i+1 <= n) {
+                oneStep = cost[i] + dp[i+1];
+            }            
+            if(i+2 <= n) {
+                twoStep = cost[i] + dp[i+2];
+            }            
+            dp[i] =  min(oneStep, twoStep);
+
+        }
+        return dp[i];        
+    }
+
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n+1, -1);
         // return min(solve(0, n,cost,), solve(1, n,cost));
-        return min(solveMem(0, n, cost, dp), solveMem(1, n,cost, dp));
+        // return min(solveMem(0, n, cost, dp), solveMem(1, n,cost, dp));
+        return min(solveTab(0, n, cost), solveTab(1, n,cost));
+        // return solve(0, n, cost);
     }
 };
