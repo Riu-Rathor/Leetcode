865c5909fb088e12bdd9ae9a04d3a6c140878b8b
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-29 16:31:13 +0530

    Time: 46 ms (46.99%) | Memory: 8.5 MB (11.57%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -24,6 +24,30 @@ class Solution {
         return dp[index][d] = finalResult;
 
     }
+
+    // tabular
+     int solve(vector<int>& jd, int n, int d) {
+        vector<vector<int>> dp(n+1, vector<int>(d+1, 0));
+        for(int i=0; i<n; i++) {
+            dp[i][1] = *max_element(begin(jd)+i, end(jd));
+        }
+        for(int days = 2; days<=d; days++) {
+            for(int index=0; index<=n-days; index++) {
+                int maxDiff = jd[index];
+                int finalResult = INT_MAX;
+                for(int i=index; i<=n-days; i++) {
+                    maxDiff = max(maxDiff, jd[i]);
+                    int result = maxDiff + dp[i+1][days-1];
+                    finalResult = min(finalResult, result);
+                }
+                 dp[index][days] = finalResult;
+            }
+        }
+        return dp[0][d];
+
+        
+
+    }
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
@@ -32,6 +56,7 @@ public:
         }
         vector<vector<int>> dp(n+1, vector<int>(d+1, -1));
         // return solve(jobDifficulty, n, 0, d);
-        return solve(jobDifficulty, n, 0, d, dp);
+        // return solve(jobDifficulty, n, 0, d, dp);
+        return solve(jobDifficulty, n, d);
     }
 };
