9ad7131d41710f1bfa62c8b2cbf101afe02da55f
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-30 11:37:59 +0530

    Time: 30 ms (5.06%) | Memory: 11.7 MB (99.61%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -3,18 +3,18 @@ public:
     bool makeEqual(vector<string>& words) {
 
         int n = words.size();
-        if(n == 1) {
-            return true;
-        }
-        unordered_map<char, int> mp;
-        for(string word : words) {
-            for(char ch : word) {
-                mp[ch]++;
+        // if(n == 1) {
+        //     return true;
+        // }
+        int freq[26] = {0};
+        for(string &word : words) {
+            for(char &ch : word) {
+                freq[ch-'a']++;
             }
         }
 
-        for(auto it : mp) {
-            if(it.second % n != 0) {
+        for(auto &it : freq) {
+            if(it % n != 0) {
                 return false;
             }
         }
