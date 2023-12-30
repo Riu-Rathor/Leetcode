163c918d03f133a583f326d66bce6b0fd482fef0
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-30 20:49:20 +0530

    Time: 405 ms (59.12%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,12 +1,11 @@
-Select t1.machine_id, ROUND(AVG(diff), 3) as processing_time from (
-select a.machine_id, a.process_id,
-(b.timestamp - a.timestamp) as diff
+
+select a.machine_id,
+ROUND(AVG(b.timestamp - a.timestamp), 3) as processing_time
 from Activity as a
 inner join Activity b
 ON a.machine_id = b.machine_id
 AND a.process_id = b.process_id
 AND a.activity_type = "start" 
 AND b.activity_type = "end"
-) t1
-GROUP BY t1.machine_id;
+GROUP BY machine_id;
 ;
