5642baf7d2898c2626a141308e31d147ba51463a
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2023-12-31 14:46:09 +0530

    Time: 1244 ms (52.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,5 @@
+Select s.user_id, 
+CASE WHEN c.time_stamp IS NULL THEN 0.00
+ELSE ROUND(SUM(c.action = 'confirmed')/COUNT(*), 2)
+END as confirmation_rate
+from Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id GROUP BY user_id;
