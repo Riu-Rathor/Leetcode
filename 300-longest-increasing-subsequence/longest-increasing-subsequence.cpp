8c9b585b43514cc53b750d0813e2456bf1c3fba4
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-05 10:45:44 +0530

    Time: 988 ms (6.75%) | Memory: 292.7 MB (11.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -37,7 +37,7 @@ class Solution {
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
         for(int curr=n-1; curr>=0; curr--) {
-            for(int prev=curr-1; prev>=-1; prev--) {
+            for(int prev=n-1; prev>=-1; prev--) {
                 int include = 0;
                 int exclude = 0;
                 if(prev == -1 || nums[curr] > nums[prev]) {
@@ -47,8 +47,7 @@ class Solution {
                 dp[curr][prev+1] = max(include, exclude);
             }
         }
-        return dp[0][0];
-        
+        return dp[0][0];        
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
