58fdb4f9d610205c9bbf8a56a8fe68744f8e65f7
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-05 10:52:23 +0530

    Time: 687 ms (20.54%) | Memory: 11.2 MB (32.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -49,12 +49,33 @@ class Solution {
         }
         return dp[0][0];        
     }
+
+    // Space optimization
+    int solveSpace(vector<int> &nums, int n) {
+        vector<int> currRow(n+1);
+        vector<int> nextRow(n+1);
+        
+        for(int curr=n-1; curr>=0; curr--) {
+            for(int prev=n-1; prev>=-1; prev--) {
+                int include = 0;
+                int exclude = 0;
+                if(prev == -1 || nums[curr] > nums[prev]) {
+                    include = 1 + nextRow[curr+1];
+                }
+                exclude = nextRow[prev+1];
+                currRow[prev+1] = max(include, exclude);
+            }
+            nextRow = currRow;
+        }
+        return nextRow[0];        
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         // return solve(nums, n, 0, -1);
         // vector<vector<int>> dp(n, vector<int>(n+1, -1));
         // return solveMem(nums, n, 0, -1, dp);
-        return solveTab(nums, n);
+        // return solveTab(nums, n);
+        return solveSpace(nums, n);
     }
 };
