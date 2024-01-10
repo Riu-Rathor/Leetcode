fe8e5441d9f039e8ec9feffe3fd3a2a04679f595
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-10 16:20:09 +0530

    Time: 10 ms (92.36%) | Memory: 11.5 MB (8.51%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(!root) {
+            return 0;
+        }
+
+        int depth = 0;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            depth++;
+            for(int i=0; i<size; i++) {
+                Node* front = q.front();
+                q.pop();
+                for(auto it : front->children) {
+                    q.push(it);
+                } 
+            }
+        }
+        return depth;
+    }
+};
