c872447dddd909e7efadcfed7345bad538d17618
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-10 16:24:52 +0530

    Time: 11 ms (80.56%) | Memory: 11.2 MB (53.87%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -24,21 +24,26 @@ public:
         if(!root) {
             return 0;
         }
-
-        int depth = 0;
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()) {
-            int size = q.size();
-            depth++;
-            for(int i=0; i<size; i++) {
-                Node* front = q.front();
-                q.pop();
-                for(auto it : front->children) {
-                    q.push(it);
-                } 
-            }
+        
+        int ans = 0;
+        for(auto it : root->children) {
+            ans = max(ans, maxDepth(it)); 
         }
-        return depth;
+        return ans + 1;
+        // int depth = 0;
+        // queue<Node*> q;
+        // q.push(root);
+        // while(!q.empty()) {
+        //     int size = q.size();
+        //     depth++;
+        //     for(int i=0; i<size; i++) {
+        //         Node* front = q.front();
+        //         q.pop();
+        //         for(auto it : front->children) {
+        //             q.push(it);
+        //         } 
+        //     }
+        // }
+        // return depth;
     }
 };
