10bdb5eb8d439f5a71e6fe47741a925891e4e841
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-18 07:30:35 +0530

    Time: 3 ms (16.28%) | Memory: 6.6 MB (18.58%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,56 +1,21 @@
 class Solution {
-public:
 
-    int solve(int n) {
+    int solve(int n, vector<int> &dp) {
         if(n == 0) {
             return 1;
-        } 
+        }
         if(n < 0) {
             return 0;
         }
 
-        return solve(n-1) + solve(n-2);
-    }
-
-    // memoization
-    int solveMem(int n, vector<int> &dp) {
-        if(n == 0 || n == 1) {
-            return 1;
-        }
         if(dp[n] != -1) {
             return dp[n];
         }
-        return dp[n] =  solveMem(n-1, dp) + solveMem(n-2, dp);        
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
-
-    // tabular method
-    int solveTab(int n) {
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i<=n; i++) {
-            dp[i] =  dp[i-1] + dp[i-2];  
-        }
-        return dp[n];
-    }
-
-    // Space optimization
-    int solveSpace(int n) {
-        // vector<int> dp(n+1, 0);
-        int prev = 1;
-        int curr = 1;
-        for(int i=2; i<=n; i++) {
-            int temp = curr;
-            curr =  prev + curr; 
-            prev = temp;
-        }
-        return curr;
-    }
+public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-        // return solve(n);
-        // return solveMem(n, dp);
-        // return solveTab(n);
-        return solveSpace(n);
+        return solve(n, dp);
     }
 };
