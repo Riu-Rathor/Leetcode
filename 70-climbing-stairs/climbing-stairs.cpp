2c16d5f14e62be42bcfc0a3c4a4887398c82030c
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-18 07:34:03 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (8.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,6 +13,17 @@ class Solution {
         }
         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
+
+    // Tabular method
+    int solveTab(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=n; i++) {
+            dp[n] = dp[n-1] + (n-2 >=0) ? dp[n-2] : 0;
+        }
+        return dp[n];
+        
+    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
