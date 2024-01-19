426b58e5f6cc56fc80baf39eff6880e1a2ae6f2c
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-19 16:48:35 +0530

    Time: 84 ms (5.06%) | Memory: 14.2 MB (5.13%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -43,13 +43,45 @@ class Solution {
         }
         return minSum;
     }
+
+    // Space Optimizer
+    int solveSpace(vector<vector<int>>& matrix, int n) {
+        // vector<vector<int>> dp(n, vector<int> (n, 0));
+        vector<int> curr(n, 0);
+        vector<int> next(n, 0);
+        for(int col = 0; col < n; col++) {
+            next[col] = matrix[n-1][col];
+        }
+        
+        
+        for(int row = n-2; row >= 0; row--) {
+            for(int col = n-1; col >= 0; col--) {
+                curr[col] = INT_MAX;
+                int sum = matrix[row][col];
+                for(int shift = -1; shift <= 1; shift++) {
+                    if(col + shift < n && col + shift >= 0 && row + 1 < n) {
+                        curr[col] = min(curr[col], sum + next[col+shift]);
+                    }
+                }
+            }
+            next = curr;
+        }
+
+        int minSum = INT_MAX;
+        for(int col = 0; col < n; col++) {
+            minSum = min(minSum, next[col]);
+        }
+        return minSum;
+    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int result = INT_MAX;
         int n = matrix.size();
-        // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -0));
         for(int j = 0; j < n; j++) {
-            result = min(result, solveTab(matrix, n));           
+            // result = min(result, solveTab(matrix, n));           
+            // result = min(result, solve(matrix, n, 0, j, dp));           
+            result = min(result, solveSpace(matrix, n));           
         }
         // result = solveTab(matrix, n);
         return result;
