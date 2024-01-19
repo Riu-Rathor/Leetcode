8c819709636e08776cc92db03d901e4e2ea7781f
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-19 18:12:36 +0530

    Time: 7 ms (32.73%) | Memory: 9.7 MB (8.59%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,16 +1,21 @@
 class Solution {
+    int solve(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        if(n%2 == 0) {
+            return solve(n/2);
+        }
+        else {
+            return solve(n/2) + 1;
+        }
+    }
 public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0; i<=n; i++) {
-            int num = i;
-            int count = 0;
-            while(num) {
-                count += num & 1;
-                num = num >> 1;
-            }
-            ans.push_back(count);
+            ans.push_back(solve(i));
         }
         return ans;
+        
     }
 };
