748c2add059d9e03bb5bd237d6eba69b64908510
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-21 07:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -21,11 +21,28 @@ class Solution {
         }
         return dp[0];    
     }
+
+    // Space optimization
+    int solveSpace(vector<int> &nums, int n) {
+        // vector<int> dp(n+1, 0);
+        int curr = 0;
+        int next1 = 0;
+        int next2 = 0;
+        for(int index=n-1; index >= 0; index--) {
+            int include = nums[index] + (index + 2 < n ? next2 : 0);
+            int exclude = next1;
+            curr = max(include, exclude);
+            next2 = next1;
+            next1 = curr;
+        }
+        return next1;    
+    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         // vector<int> dp(n+1, -1);
         // return solve(nums, n, 0, dp);
-        return solveTab(nums, n);
+        // return solveTab(nums, n);
+        return solveSpace(nums, n);
     }
 };
