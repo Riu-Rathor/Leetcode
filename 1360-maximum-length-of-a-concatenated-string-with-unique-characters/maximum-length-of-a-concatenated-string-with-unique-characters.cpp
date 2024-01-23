d6db5614f551dc82073e1f414f95e433595594f8
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-23 07:15:05 +0530

    Time: 101 ms (43.95%) | Memory: 110.8 MB (20.38%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int solve(int index, int n, vector<string> &arr, unordered_map<char, int> &mp) {
+        if(index >= n) {
+            return 0;
+        }
+        int include = 0;
+        int exclude = 0;
+        bool isPresent = false;
+        unordered_map<char, int> temp;
+        for(int i=0; i<arr[index].length(); i++) {
+            if(mp.find(arr[index][i]) != mp.end()) {
+                isPresent = true;
+                break;
+            }
+            if(temp.find(arr[index][i]) != temp.end()) {
+                isPresent = true;
+                break;
+            }
+            temp[arr[index][i]]++;            
+        }
+
+        if(!isPresent) {
+            for(int i=0; i<arr[index].length(); i++) {
+                mp[arr[index][i]]++;
+            }
+            include = arr[index].length() + solve(index+1, n, arr, mp);
+            for(int i=0; i<arr[index].length(); i++) {
+                mp.erase(arr[index][i]);
+            }
+        }
+        exclude = solve(index+1, n, arr, mp);
+        return max(include, exclude);
+    }
+public:
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        unordered_map<char, int> mp;
+        return solve(0, n, arr, mp);
+    }
+};
