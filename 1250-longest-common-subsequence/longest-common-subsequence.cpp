64477f4387d6f91e3c4ba6aefba8cb30ca67f93a
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-25 07:55:48 +0530

    Time: 22 ms (88.88%) | Memory: 7.8 MB (87.29%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -38,12 +38,39 @@ class Solution {
         return dp[0][0];
         
     }
+
+    // Space Optimization
+    int solveSpace(string a, string b) {
+        int n1 = a.length();
+        int n2 = b.length();
+
+        vector<int>curr(n2+1, 0);
+        vector<int> next(n2+1, 0);
+
+        for(int i=n1-1; i>=0; i--) {
+            for(int j=n2-1; j>=0; j--) {
+                int matches = 0;
+                int notMatches = 0;
+                if(a[i] == b[j]) {
+                    matches = 1 + next[j+1];
+                }
+                else {
+                    notMatches = max(next[j], curr[j+1]);
+                }
+                curr[j] = max(matches, notMatches);
+            }
+            next = curr;
+        }
+        return curr[0];
+        
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.length();
         int n2 = text2.length();
         // vector<vector<int>> dp(n1, vector<int>(n2, -1));
         // return solve(0, 0, text1, text2, dp);
-        return solveTab(text1, text2);
+        // return solveTab(text1, text2);
+        return solveSpace(text1, text2);
     }
 };
