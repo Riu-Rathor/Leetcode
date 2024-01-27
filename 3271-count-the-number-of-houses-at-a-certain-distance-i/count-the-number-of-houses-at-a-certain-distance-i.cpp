fc4bfa03c6d710a8dbc263533f9e9f26411fbe49
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-01-27 11:11:03 +0530

    Time: 566 ms (8.51%) | Memory: 18.4 MB (37.31%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -26,6 +26,9 @@ public:
         vector<int> result(n);
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
+                if(i == j) {
+                    continue;
+                }
                 if(grid[i][j]) {
                     result[grid[i][j]-1]++;
                 }
