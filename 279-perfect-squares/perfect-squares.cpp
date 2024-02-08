3d141cfe5c163164eeae004a0591704618bcbb6a
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-08 18:39:27 +0530

    Time: 75 ms (74.32%) | Memory: 10.4 MB (38.33%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,5 +1,6 @@
 class Solution {
 
+// Recursion + Memoization
     int solve(int n, vector<int> &dp) {
         if(n <= 0) {
             return 0;
@@ -15,9 +16,26 @@ class Solution {
         }
         return dp[n] = ans;
     }
+
+    // Bottom up
+    int solveTab(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int j=1; j<=n; j++) {
+            int ans = INT_MAX;
+            for(int i=1; i<=sqrt(j); i++) {
+                int temp = 1 + dp[j-(i*i)];
+                ans = min(ans, temp);            
+            }
+            dp[j] = ans;
+        }
+        return dp[n];        
+    }
+
 public:
     int numSquares(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return solve(n, dp);
+        return solveTab(n);
     }
 };
