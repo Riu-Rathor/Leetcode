2f0f4f4ec7e732f5fea9498af3dbb7fbc6eb0ffc
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-11 15:00:49 +0530

    Time: 65 ms (69.23%) | Memory: 20.5 MB (6.58%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -103,8 +103,53 @@ class Solution {
                 }
             }
         }
-        return dp[0][0][cols-1];
-        
+        return dp[0][0][cols-1];        
+    }
+
+    // Space optimixation
+    int solveSpace(vector<vector<int>>& grid, int n, int cols) {
+        vector<vector<int>> curr(cols, vector<int>(cols, 0));
+        vector<vector<int>> next(cols, vector<int>(cols, 0));
+        for(int j1 = 0; j1 < cols; j1++) {
+            for(int j2 = 0; j2 < cols; j2++) {
+                if(j1 == j2) {
+                    next[j1][j2] = grid[n-1][j1];
+                }
+                else {
+                    next[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                }                
+            }
+        }
+
+        for(int i = n-2; i >= 0; i--) {
+            for(int j1 = 0; j1 < cols; j1++) {
+                for(int j2 = 0; j2 < cols; j2++) {
+                    int maxi = -1e8;
+                    for(int dj1 = -1; dj1 <= 1; dj1++) {
+                        for(int dj2 = -1; dj2 <= 1; dj2++) {
+                            int value = 0;
+                            if(j1 == j2) {
+                                value = grid[i][j1];
+                            }
+                            else {
+                                value = grid[i][j1] + grid[i][j2];
+                            }
+
+                            if(j1+dj1 >= 0 && j1+dj1 < cols && j2+dj2 >= 0 && j2+dj2 < cols) {
+                                value += next[j1+dj1][j2+dj2];
+                            }
+                            else {
+                                value += -1e8;
+                            }
+                            maxi = max(maxi, value);
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            next = curr;
+        }
+        return curr[0][cols-1];        
     }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
@@ -113,6 +158,7 @@ public:
         vector<vector<vector<int>>> dp(rows+1, vector<vector<int>>(cols+1, vector<int>(cols+1, -1)));
         // return solve(0, 0, cols-1, grid, rows, cols);
         // return solveMem(0, 0, cols-1, grid, rows, cols, dp);
-        return solveTab(grid, rows, cols);
+        // return solveTab(grid, rows, cols);
+        return solveSpace(grid, rows, cols);
     }
 };
