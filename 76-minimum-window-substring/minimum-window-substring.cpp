cfa95c78a70b08f88c26acc4aa201c7dcf6fa727
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-11 11:54:48 +0530

    Time: 17 ms (56.54%) | Memory: 10.5 MB (42.15%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty()) {
+        int n = s.length();
+        int countRequired = t.length();
+        if(countRequired > n) {
             return "";
         }
-
-        unordered_map<char, int> dictT;
-        for (char c : t) {
-            int count = dictT[c];
-            dictT[c] = count + 1;
+        unordered_map<char, int> mp;
+        // store the characters of string t in map 
+        for(char &ch : t) {
+            mp[ch]++;
         }
 
-        int required = dictT.size();
-        int l = 0, r = 0;
-        int formed = 0;
+        int i = 0;
+        int j = 0;
+        int windowSize = INT_MAX;
+        int start_i = 0;
 
-        unordered_map<char, int> windowCounts;
-        int ans[3] = { -1, 0, 0 };
-
-        while (r < s.length()) {
-            char c = s[r];
-            int count = windowCounts[c];
-            windowCounts[c] = count + 1;
-
-            if (dictT.find(c) != dictT.end() && windowCounts[c] == dictT[c]) {
-                formed++;
+        while(j < n ) {
+            char ch = s[j];
+            if(mp[ch] > 0) {
+                countRequired--;
             }
-
-            while (l <= r && formed == required) {
-                c = s[l];
-
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+            mp[ch]--;
+            while(countRequired == 0) {
+                // shrink the window
+                int currWindowSize = j - i + 1;
+                if(windowSize > currWindowSize) {
+                    windowSize = currWindowSize;
+                    start_i = i;
                 }
 
-                windowCounts[c]--;
-                if (dictT.find(c) != dictT.end() && windowCounts[c] < dictT[c]) {
-                    formed--;
+                mp[s[i]]++;
+                if(mp[s[i]] > 0) {
+                    countRequired++;
                 }
-
-                l++;
+                i++;
             }
-
-            r++;
+            j++;
         }
 
-        return ans[0] == -1 ? "" : s.substr(ans[1], ans[0]);
+        return windowSize == INT_MAX ? "" : s.substr(start_i, windowSize);
     }
 };
