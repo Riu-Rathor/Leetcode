b706cc197efbbbda95ed5a5620a7930f9f4c7a4d
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-17 14:45:31 +0530

    Time: 71 ms (83.21%) | Memory: 57.5 MB (21.59%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int n = heights.size();
+        priority_queue<int> pq;
+        int i = 0;
+        for(; i<n-1; i++) {
+            if(heights[i+1] <= heights[i]) {
+                continue;
+            }
+            int diff = heights[i+1] - heights[i];
+            if(bricks >= diff) {
+                bricks -= diff;
+                pq.push(diff);
+            }
+            else if(ladders > 0){
+                if(!pq.empty()){
+                    int max_bricks_past = pq.top();
+                    if(max_bricks_past > diff) {
+                        bricks += max_bricks_past;
+                        pq.pop();
+                        bricks -= diff;
+                        pq.push(diff);
+                    }
+                }
+                ladders--;
+            }
+            else {
+                break;
+            }
+        }
+        return i;
+    }
+};
