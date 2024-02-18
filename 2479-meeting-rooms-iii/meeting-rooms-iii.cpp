19523087ece39e8b72056d163a143bf7060071b7
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-18 17:56:23 +0530

    Time: 280 ms (98.15%) | Memory: 100.1 MB (75.66%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    typedef pair<long long, int> P;
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        int m = meetings.size();
+        sort(meetings.begin(), meetings.end());
+        vector<int> roomUsedCount(n, 0);
+        priority_queue<P, vector<P>, greater<P>> usedRooms;
+        priority_queue<int, vector<int>, greater<int>> availableRooms;
+        for(int room=0; room<n; room++) {
+            availableRooms.push(room);
+        }
+
+        for(vector<int> &meet : meetings) {
+            int start = meet[0];
+            int end = meet[1];
+            int duration = end - start;
+            while(!usedRooms.empty() && usedRooms.top().first <= start) {
+                int room = usedRooms.top().second;
+                usedRooms.pop();
+                availableRooms.push(room);
+            }
+
+            if(!availableRooms.empty()) {
+                int room = availableRooms.top();
+                availableRooms.pop();
+                usedRooms.push({end, room});
+                roomUsedCount[room]++;
+            }
+            else {
+                int room = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+                usedRooms.pop();
+                usedRooms.push({endTime+duration, room});
+                roomUsedCount[room]++;
+            }
+        }
+        int resultRoom = -1;
+        int maxUse = 0;
+
+        for(int room=0; room<n; room++) {
+            if(roomUsedCount[room] > maxUse) {
+                resultRoom = room;
+                maxUse = roomUsedCount[room];
+            }
+        }
+        return resultRoom;
+
+    }
+
+};
