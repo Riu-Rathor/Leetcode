43ae47b55fe743ee110a389c607eea050180b91e
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-02-22 21:11:10 +0530

    Time: 119 ms (87.72%) | Memory: 64.3 MB (62.99%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int ans = -1; 
         int size = trust.size();
         vector<pair<int, int>> arr(n+1, {0, 0});
-
         for(int i=0; i<size; i++) {
-            arr[trust[i][0]].first++; 
-            arr[trust[i][1]].second++; 
+            arr[trust[i][0]].first++;
+            arr[trust[i][1]].second++;
         }
 
-
         for(int i=1; i<=n; i++) {
-            if(arr[i].first == 0 && arr[i].second == n-1) {
+            if(arr[i].first == 0 && arr[i].second == n - 1) {
                 return i;
             }
         }
-        return ans;
+        return -1;
     }
-};
+}; 
