05e61d0e561be0876c11f43c6757c500986720f9
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-04-24 14:54:13 +0530

    Time: 3 ms (22.77%) | Memory: 7.3 MB (13.08%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,23 +1,37 @@
 class Solution {
-public:
-    int tribonacci(int n) {
-        int one = 0;
-        int two = 1;
-        int three = 1; 
+
+    // Recursion 
+    int solve(int n) {
         if(n == 0) {
-            return one;
+            return 0;
         }
 
         if(n == 1 || n == 2) {
-            return two;
+            return 1;
         }
-        long long  ans = 0;
-        for(int i=0; i<= n - 3; i++) {
-            ans = one + two + three;
-            one = two;
-            two = three;
-            three = ans;
+
+        return solve(n-3) + solve(n-2) + solve(n-1);
+    }
+
+    // Memoization
+    int solveMem(int n, vector<int> &dp) {
+        if(n == 0) {
+            return 0;
         }
-        return ans;
+
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = solveMem(n-3, dp) + solveMem(n-2, dp) + solveMem(n-1, dp);
+    }
+
+
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+        return solveMem(n, dp);
     }
 };
