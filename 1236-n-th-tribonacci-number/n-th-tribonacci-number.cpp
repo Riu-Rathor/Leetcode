80c655d5d7a364ce1ba8cf0dcfcaf8d203734648
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-04-24 15:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -29,27 +29,32 @@ class Solution {
     }
 
 
-    // Tabular solution
-    int solveTab(int n) {
+    // Space Optimixation
+    int solveSpace(int n) {
         if(n == 0) {
             return 0;
         }
-        if(n == 1) {
+        if(n == 1 || n == 2) {
             return 1;
         }
-        vector<int> dp(n+1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+
+        int one = 0;
+        int two = 1;
+        int three = 1;
+        long long ans = 0;
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            ans = one + two + three;
+            one = two;
+            two = three;
+            three = ans;
         }        
-        return dp[n];
+        return ans;
     }
 public:
     int tribonacci(int n) {
         // vector<int> dp(n+1, -1);
         // return solveMem(n, dp);
-        return solveTab(n);
+        // return solveTab(n);
+        return solveSpace(n);
     }
 };
