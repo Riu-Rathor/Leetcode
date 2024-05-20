e6d68bd92fc7c20ea14fbf20fa71d0bc93729cc0
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-05-20 11:18:25 +0530

    Time: 199 ms (45.95%) | Memory: 129 MB (39.64%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
 typedef long long ll;
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        ll sum = 0;
-        int count = 0;
-        int minNuksan = INT_MAX;
+        vector<int> fayda;
+        ll normalSum = 0;
 
-        for(ll num: nums) {
-            if((num^k) > num) {
-                count++;
-                sum += (num^k);
-            }
-            else {
-                sum += num;
-            }
-            minNuksan = min((long long)minNuksan, abs(num - (num ^ k)));
+        for(int i=0; i<nums.size(); i++) {
+            normalSum += nums[i];
+            fayda.push_back((nums[i]^k) - nums[i]);
         }
 
-        if(count % 2 == 0) {
-            return sum;
+        sort(fayda.begin(), fayda.end(), greater<int>());
+        int n = fayda.size();
+        for(int i=0; i<n-1; i+=2) {
+            ll pairSum = fayda[i] + fayda[i+1];
+            if(pairSum > 0) {
+                normalSum += pairSum;
+            }
         }
-        return sum - minNuksan;
-    } 
+        return normalSum;
+    }
 };
