a5f1ca69c6def8b56cabe93c3ac0479046f03392
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-05-26 18:24:27 +0530

    Time: 1300 ms (14.59%) | Memory: 397.6 MB (38.35%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -19,6 +19,28 @@ class Solution {
         return dp[n][absent][late] = ((A + L) % MOD + P) % MOD;
   }
 
+int solveTab(int n, int absent, int late) {
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<3; j++) {
+                dp[0][i][j] = 1;
+            }
+        }
+
+        for(int i=n; i>=0; i--) {
+            for(int absent=0; absent<2; absent++) {
+                for(int late=0; late<3; late++) {
+                    int P = dp[n-1][absent][0] % MOD;
+                    int A = dp[n-1][absent+1][0] % MOD;
+                    int L = dp[n-1][absent][late+1] % MOD;
+                    dp[n][absent][late] = ((A + L) % MOD + P) % MOD;
+                }
+            }
+        }
+        return dp[n][0][0];
+
+        
+  }
 public:
     int checkRecord(int n) {
         int late = 0;
