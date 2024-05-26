2dd277d46bbb091992578162e96572ec2db2d3b0
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-05-26 18:50:59 +0530

    Time: 1061 ms (34.00%) | Memory: 386.6 MB (44.25%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -21,31 +21,30 @@ class Solution {
 
 int solveTab(int n, int absent, int late) {
         vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-        for(int i=0; i<2; i++) {
-            for(int j=0; j<3; j++) {
+        for(int i=0; i<2; ++i) {
+            for(int j=0; j<3; ++j) {
                 dp[0][i][j] = 1;
             }
         }
 
-        for(int i=n; i>=0; i--) {
+        for(int i=1; i<=n; i++) {
             for(int absent=0; absent<2; absent++) {
                 for(int late=0; late<3; late++) {
-                    int P = dp[n-1][absent][0] % MOD;
-                    int A = dp[n-1][absent+1][0] % MOD;
-                    int L = dp[n-1][absent][late+1] % MOD;
-                    dp[n][absent][late] = ((A + L) % MOD + P) % MOD;
+
+                    long long result = dp[i-1][absent][0];
+                    result += (absent == 0 ? dp[i-1][absent+1][0] : 0);
+                    result += (late < 2 ? dp[i-1][absent][late+1] : 0);
+                    dp[i][absent][late] = result % MOD;
                 }
             }
         }
-        return dp[n][0][0];
-
-        
+        return dp[n][0][0];        
   }
 public:
     int checkRecord(int n) {
         int late = 0;
         int absent = 0;
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(n, 0, 0, dp);
+        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return solveTab(n, 0, 0);
     }
 };
