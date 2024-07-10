7a68cfb058c235a861da48c698355ddac407e70e
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-07-10 07:38:31 +0530

    Time: 4 ms (67.13%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,21 +1,16 @@
 class Solution {
+
 public:
     int minOperations(vector<string>& logs) {
-        int n = logs.size();
-        int ans = 0;
-        for(string log : logs) {
-            int dotCount = 0;
-            if(log[0] == '.') dotCount++;
-            if(log[1] == '.') dotCount++;
-            if(dotCount == 0) {
-                ans++;
-            } else if(dotCount == 2) {
-                if(ans - 1 >= 0) {
-                    ans--;
-                }
+        int count = 0;
+        for(string log : logs) { 
+            if(log[0] >= 'a' && log[0] <= 'z' || log[0] >= '1' && log[0] <= '9' || log[0] >= 'A' && log[0] <= 'Z') {
+                count++;
+            }
+            else if(log[0] == '.' && log[1] == '.') {
+                count = (count > 0) ? count - 1 : 0;
             }
         }
-        return ans;
-
+        return count;
     }
 };
