c346bb804a24fa425d05d31b70311cbbfac09913
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-07-10 10:42:03 +0530

    Time: 118 ms (64.24%) | Memory: 101 MB (74.79%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        deque<int> deque;
-        
-        vector<int> res(temperatures.size(), 0);
-
-        for (int i = temperatures.size() - 1; i >= 0; --i) {
-            if (deque.empty()) {
-                deque.push_front(i);
-                res[i] = 0;
-            } else {
-                while (!deque.empty() && temperatures[i] >= temperatures[deque.front()]) {
-                    deque.pop_front();
-                }
-
-                if (deque.empty()) {
-                    res[i] = 0;
-                } else {
-                    res[i] = deque.front() - i;
-                }
-
-                deque.push_front(i);
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+                st.pop();
             }
-        }
 
-        return res;
+            if(st.empty()) {
+                ans[i] = 0;
+            }
+            else {
+                ans[i] = st.top() - i;
+            }
+
+            st.push(i);
+        }
+        return ans;
     }
 };
