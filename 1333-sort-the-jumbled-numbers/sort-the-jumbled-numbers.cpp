60d94a96415b2a498c98736a68e3e3a2dd44f651
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-07-24 14:09:02 +0530

    Time: 126 ms (97.40%) | Memory: 87.6 MB (67.53%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -3,14 +3,20 @@ public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         vector<pair<int, int>> newNums;
         for(int i=0; i<nums.size(); i++) {
-            string temp = to_string(nums[i]);
-            string newTemp = "";
-            for(int j=0; j<temp.size(); j++) {
-                int index = (temp[i]);
-                newTemp = newTemp + to_string(mapping[temp[j] - '0']);
+            int newTemp = 0;
+            int temp = nums[i];
+            int place = 1;
+            if(temp == 0) {
+                newNums.push_back({mapping[0], nums[i]});
+                continue;
             }
 
-            newNums.push_back({stoi(newTemp), nums[i]});
+            while(temp != 0) {
+                newTemp = place * mapping[temp%10] + newTemp;
+                place *= 10;
+                temp /= 10;
+            }
+            newNums.push_back({newTemp, nums[i]});
         }
         auto lambda = [] (pair<int, int> &p1, pair<int, int> &p2) {
             return p1.first < p2.first;
