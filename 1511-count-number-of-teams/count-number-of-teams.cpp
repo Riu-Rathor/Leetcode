955d899ea838dd1e2c6e593ea479b68575d2eb7e
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-07-29 11:42:10 +0530

    Time: 27 ms (80.51%) | Memory: 11.7 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size();
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                for(int k=j+1; k<n; k++) {
-                    if((rating[i] > rating[j] && rating[j] > rating[k]) || (rating[i] < rating[j] && rating[j] < rating[k])) {
-                        ans++;
-                    }
+        int teams = 0;
+        for(int j=1; j<n-1; j++) {
+            int countSmallerLeft = 0;
+            int countLargerLeft = 0;
+
+            for(int i=0; i<j; i++) {
+                if(rating[i] < rating[j]) {
+                    countSmallerLeft++;
+                }
+                else if(rating[i] > rating[j]) {
+                    countLargerLeft++;
                 }
             }
+
+            int countSmallerRight = 0;
+            int countLargerRight = 0;
+
+            for(int i=j+1; i<n; i++) {
+                if(rating[i] < rating[j]) {
+                    countSmallerRight++;
+                }
+                else if(rating[i] > rating[j]) {
+                    countLargerRight++;
+                }
+            }
+
+            teams += (countSmallerLeft * countLargerRight);
+            teams += (countSmallerRight * countLargerLeft);
         }
-        return ans;
+        return teams;
     }
 };
