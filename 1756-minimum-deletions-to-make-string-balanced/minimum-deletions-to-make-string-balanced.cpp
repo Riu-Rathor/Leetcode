d7884cafc2484614bcfe7de16c49db3c85051e7b
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-07-30 11:04:43 +0530

    Time: 86 ms (66.83%) | Memory: 23.5 MB (80.38%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.length();
-        vector<int> rightCountA(n, 0);
-        int countB = 0;
         int countA = 0;
+        int countB = 0;
         for(int i=n-1; i>=0; i--) {
-            rightCountA[i] = countA;
             if(s[i] == 'a') {
                 countA++;
             }
@@ -14,7 +12,10 @@ public:
 
         int minimumDeletion = INT_MAX;
         for(int i=0; i<n; i++) {
-            minimumDeletion = min(minimumDeletion, (rightCountA[i] + countB));
+            if(s[i] == 'a') {
+                countA--;
+            }
+            minimumDeletion = min(minimumDeletion, (countA + countB));
             if(s[i] == 'b') {
                 countB++;
             }
