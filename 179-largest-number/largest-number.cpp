78acec539f88a53e83c00533f8740342ae3e9c7d
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2024-09-18 07:29:55 +0530

    Time: 14 ms (7.96%) | Memory: 15.7 MB (70.19%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
+    bool static comparator (int num1, int num2) {
+            string temp1 = to_string(num1);
+            string temp2 = to_string(num2);
+            return temp1 + temp2 > temp2 + temp1;
+        }
 public:
-
-    bool static comparator(int a, int b) {
-        string temp1 = to_string(a);
-        string temp2 = to_string(b);
-        return temp1 + temp2 > temp2 + temp1;
-    }
     string largestNumber(vector<int>& nums) {
-        int n = nums.size();
-        string ans = "";
+        string result = "";
+        
         sort(nums.begin(), nums.end(), comparator);
         if(nums[0] == 0) {
             return "0";
         }
 
-        for(int i=0; i<n; i++) {
-            ans += to_string(nums[i]);
+        for(int i=0; i<nums.size(); i++) {
+            result += to_string(nums[i]);
         }
-        return ans;
+        return result;
     }
 };
