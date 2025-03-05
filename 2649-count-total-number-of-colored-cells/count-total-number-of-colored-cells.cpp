c88582c8b541d69d237b2408482339e33f1b89d2
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2025-03-05 09:49:02 +0530

    Time: 40 ms (5.86%) | Memory: 44.2 MB (5.25%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,9 +1,16 @@
 class Solution {
-public:
-    long long coloredCells(int n) {
+    long long solve(int n, vector<long long> &dp) {
         if(n == 1) {
             return 1;
         }
-        return coloredCells(n-1) + 4*(n-1);
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = solve(n-1, dp) + 4*(n-1);
+    }
+public:
+    long long coloredCells(int n) {
+        vector<long long> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
