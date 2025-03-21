79089116e3792dcbd31cc1cb25c81e3916b80bb5
Riya Saini <81743211+Riu-Rathor@users.noreply.github.com>
2025-03-21 14:15:21 +0530

    Time: 31 ms (95.55%) | Memory: 101.2 MB (88.82%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        int n = recipes.size();
+        unordered_map<string, vector<int>> adj;
         unordered_set<string> st(supplies.begin(), supplies.end());
-
-        vector<string> result;
-        int lastSize = -1;
-
+        vector<int> indegree(n, 0);
         queue<int> q;
-        for(int i=0; i<recipes.size(); i++) {
-            q.push(i);
-        }
- 
-        while(static_cast<int>(st.size()) > lastSize) {
-            lastSize = st.size();
-            int size = q.size();
+        vector<string> result;
 
-            while(size--) {
-                int idx = q.front();
-                bool canCreate = true;
-                q.pop();
 
-                 for(string &ingredient : ingredients[idx]) {
-                    if(!st.count(ingredient)) {
-                        canCreate = false;
-                        break;
-                    }
+        for(int i=0; i<n; i++) {
+            for(string &ingredient : ingredients[i]) {
+                if(!st.count(ingredient)) {
+                    adj[ingredient].push_back(i);
+                    indegree[i]++;
                 }
+            }
+        }
 
-                if(!canCreate) {
+        for(int i=0; i<n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int i = q.front();
+            q.pop();
+            string recipe = recipes[i];
+            result.push_back(recipe);
+
+            for(int &idx : adj[recipe]) {
+                indegree[idx]--;
+                if(indegree[idx] == 0) {
                     q.push(idx);
                 }
-                else {
-                    st.insert(recipes[idx]);
-                    result.push_back(recipes[idx]);
-                }
-            }           
+            }
         }
         return result;
     }
